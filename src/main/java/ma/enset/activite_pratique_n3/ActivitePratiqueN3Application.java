c2700f361e0d8e81121663cc047cd16588f9b087
unknown <sara>
2024-03-23 09:43:25 +0000

    commit V1

---
+++ src/main/java/ma/enset/activite_pratique_n3/ActivitePratiqueN3Application.java
@@ -0,0 +1,36 @@
+package ma.enset.activite_pratique_n3;
+
+import ma.enset.activite_pratique_n3.entities.Patient;
+import ma.enset.activite_pratique_n3.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class ActivitePratiqueN3Application implements CommandLineRunner {
+
+    @Autowired
+    PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitePratiqueN3Application.class, args);
+    }
+
+    //  @Override
+
+    public void run(String... args) throws Exception {
+        /*Patient p1=new Patient();
+        p1.setId(null);
+        p1.setNom("aya");
+        p1.setDateN(new Date());
+        p1.setMalade(false);
+        p1.setScore(120);
+
+        patientRepository.save(p1);*/
+
+
+    }
+}
